9dca9da2b2be03b17abf922af11bf02e26c920f4
Gexedd <eeviac.cl@gmail.com>
2023-02-05 19:03:51 -0300

    Se sube el proyecto peliculas desarrollado en flutter

---
+++ android/app/src/main/kotlin/eduardoevia/com/proyecto_peliculas/MainActivity.kt
@@ -0,0 +1,6 @@
+package eduardoevia.com.proyecto_peliculas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_peliculas/screens/screens.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      title: 'Peliculas',
+      initialRoute: 'home',
+      routes: {
+        'home':( _ )=> HomeScreen(),
+        'details':( _ )=> DetailsScreen(),
+      },
+
+    );
+  }
+}
