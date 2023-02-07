e34452244069fa3b8f660e25175e5b2e620c8cf6
Gexedd <eeviac.cl@gmail.com>
2023-02-07 10:12:43 -0300

    Se crea el widget _PosterAndTitle para ser invocado en el delegate de SilverChildListDelegate. Se crean los textos en el row del poster

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,14 +1,104 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+//TODO: Cambiar por una instancia de The movie
+
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? "no-movie";
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
-        ),
+        body: CustomScrollView(
+      slivers: [
+        _CustomAppBar(),
+        //Añadiendo cosas para que se vean debajo
+        //Deben ser del tipo sliver list
+        SliverList(
+            delegate: SliverChildListDelegate(
+                [Text("Hola Pelicula"), _PosterAndTitle()]))
+      ],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+        backgroundColor: Colors.indigo,
+        expandedHeight: 200,
+        floating: false,
+        pinned: true,
+        flexibleSpace: FlexibleSpaceBar(
+          centerTitle: true,
+          titlePadding: EdgeInsets.all(0),
+          title: Container(
+              width: double.infinity,
+              alignment: Alignment.bottomCenter,
+              color: Colors
+                  .black26, //Oscurece un poco la foto para que se vea mejor el texto de la pelicula
+              child: Text(
+                "movie.title",
+                style: TextStyle(fontSize: 20),
+              )),
+          background: FadeInImage(
+            placeholder: AssetImage("assets/loading.gif"),
+            image: NetworkImage(
+                "http://mireflex.sytes.net/cinemaplus2/posters/943c34f65a87fb246b5069ce4c11df46.jpg"),
+            fit: BoxFit.fitWidth,
+          ),
+        ));
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        //Creo Rows para tener elementos uno al lado del otro
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage(
+                  "http://mireflex.sytes.net/cinemaplus2/posters/943c34f65a87fb246b5069ce4c11df46.jpg"),
+              height: 180,
+            ),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "movie-title",
+                style: Theme.of(context).textTheme.headlineMedium,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                "original-title",
+                style: Theme.of(context).textTheme.headlineSmall,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+
+              Row(
+                children: [
+                  Icon(Icons.star, size: 15,color: Colors.amber),
+                  SizedBox(width: 10),
+                  Text("movie.voteAverage", style: Theme.of(context).textTheme.bodySmall,)
+                ],
+              )
+
+            ],
+          )
+        ],
       ),
     );
   }
